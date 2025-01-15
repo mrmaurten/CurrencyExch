72720ca0322d03d1dec3b812f488a7a6de2e3d44
Mårten Öhman <marten.ohman@gmail.com>
2025-01-15 16:05:05 +0100

    first version, attempt on the menu

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -1,7 +1,38 @@
-package se.lexicon;
 
-public class Main {
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class CurrencyExchange {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
+        Scanner scanner = new Scanner(System.in);
+        CurrencyExchange exchange = new CurrencyExchange();
+        boolean running = true;
+
+        while (running) {
+            System.out.println(" --Welcome to the Currency Exchange!-- ");
+            System.out.println("1. Exchange SEK to USD");
+            System.out.println("2. Exchange SEK to EUR");
+            System.out.println("3. Exchange USD to SEK");
+            System.out.println("4. Exchange USD to EUR");
+            System.out.println("5. Exchange EUR to SEK");
+            System.out.println("6. Exchange EUR to USD");
+            System.out.println("7. Exit the exchange");
+            System.out.print("Choose an option (1-7): ");
+
+            int choice = getValidChoice(scanner);
+
+            if (choice == 7) {
+                System.out.println("Exiting the program. Thanks and goodbye!");
+                running = false;
+            } else {
+                System.out.println("Enter the amount:");
+                double amount = getValidAmount(scanner);
+                double convertedAmount = converter.convertCurrency(choice, amount);
+                if (convertedAmount != -1) {
+            }
+            }
+
+
+        }
 }
